31988b324a690a88c726a3b1cbc0ed4573232b4b
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-05-30 13:17:22 -0700

    done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,45 +8,16 @@ const Hero = () => {
      
     <div className='app__main__hero'>  
      <div className='app__hero__heading'>
-     <motion.div
-     animate={{y: [-100, 0], opacity: [0, 1]}}
-     transition={{duration: 5.5}}
-     >
-     <Three />
-     </motion.div>
-     
+     <Three />     
      <div className='app__hero__move'>
-
-     <motion.div
-     
-     animate={{y: [-100, 0], opacity: [0, 1]}}
-     transition={{duration: 1.5}}
-     >
-     
       <h1 className='app__h1'>I'm Alexsander Gom<span className='span__name'>es</span></h1>
-     </motion.div>
-      <motion.div
-         animate={{ x: [-200, 0], opacity: [0, 1] }}
-      transition={{ duration: 1 }}
-      >
       <h3 className='app__h3'>The perfect full stack developer</h3>
-       </motion.div>
-       <motion.div
-        animate={{ x: [200, 0], opacity: [0, 1] }}
-        transition={{ duration: 2 }}
-       >
       <h4 className='app__h4'>For your bussiness</h4>
-       </motion.div>
        <div className='app__contact'>
         <a href="mailto:sander.alex0909@gmail.com" target='_blank' rel="noreferrer" className='app__contact__text'>Email</a>
         <a href="tel:+1 (510) 630-5188"   className='app__contact__call'>Text me</a>
        </div>
      </div>
-
-     <motion.div
-      animate={{ x: [-200, 0], opacity: [0, 1] }}
-      transition={{ duration: 4 }}
-     >
       <div className='app__left__text'>
         <img className='app__left__img' src={'/bulb.svg'} alt=""  height={50} width={50}/>
         <div className='app__left__move'>
@@ -55,11 +26,6 @@ const Hero = () => {
         <h6 className='app__left__h6'>To Your Idea</h6>
         </div>
       </div>
-     </motion.div>
-     <motion.div
-         animate={{ x: [200, 0], opacity: [0, 1] }}
-      transition={{ duration: 4 }}
-     >
       <div className='app__right__text'>
          <img className='app__right__img' src={'/face.svg'} alt="" height={50} width={50} />
          <div className='app__right__move'>
@@ -68,7 +34,7 @@ const Hero = () => {
            <h6 className='app__right__h6'>Priority</h6>
          </div>
       </div>
-     </motion.div>
+
      </div>
      {/* <Image className='img__app' src="" alt="" /> */}
     </div>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -5,36 +5,12 @@ const Skills = () => {
   return (
     <div>
         <div className='app__hero__skills'>
-        <motion.div
-            animate={{y: [-100, 0], x: [100, 0], opacity: [0, 1]}}
-            transition={{duration: 4.9}}
-        >
         <img className='app__figma' src={'/figma.png'} alt="" />
          <div className='app__figma__circle'/> 
-        </motion.div>
-
-        <motion.div
-          animate={{y: [-100, 0], x:[-100, 0], opacity: [0, 1]}}
-          transition={{duration: 3.9}}
-        >
         <img className='app__react' src={'/react.png'} alt="" />
-        </motion.div>
-
-        <motion.div
-          animate={{y: [-100, 0], x: [100, 0], opacity: [0, 1]}}
-          transition={{duration: 3.9}}
-        >
         <img  className='app__sass' src={'/sass.png'} alt="" />
         <div className='app__sass__circle' />
-        </motion.div>
-
-        <motion.div
-          animate={{y: [-100, 0], x:[-100, 0], opacity: [0, 1]}}
-          transition={{duration: 4.9}}
-        >
-        <img className='app__node' src={'/node.svg'} alt="" />
-        </motion.div>
-  
+        <img className='app__node' src={'/node.svg'} alt="" />  
      </div>
     </div>
   )
