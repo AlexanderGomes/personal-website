a4c0a583f4bad2f2c56d257aa7f4c6b9d735114d
Alexsander Gomes baptista <sander@Alexsanders-MacBook-Pro.local>
2022-06-01 20:27:58 -0700

    done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,6 +10,10 @@ const Hero = () => {
         <h4 className='app__hero'> <span className=''>I'm</span> Alexsander Gom<span className='span__name'>es</span></h4>
         <h5 className='app__hero__2'>The best Full-Stack developer</h5>
         <h6 className='app__hero__3'>for your bussines</h6>
+        <div className='btns'>
+       <a href="tel:+1 (510) 630-5188" > <button className='btn__1'>Call</button> </a>
+        <a href="mailto: sander.alex0909@gmail.com"><button className='btn__2'>email me</button></a>
+        </div>
       </div>
     </div>
      
